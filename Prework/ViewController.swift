71bd928c4ce4d199ef744742873b04370eae88b2
shannonsuhendra <shannon@suhendra.com>
2021-08-31 12:56:38 -0400

    Initial Commit

---
+++ Prework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Shannon Suhendra on 31/08/21.
+//  Copyright © 2021 Shannon Suhendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
